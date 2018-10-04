8b5bca9a91c33c4b87d5b331fbe1b4a65f5dadd0
KyriaElixia <kristianjandric@gmail.com>
2018-10-04 19:14:21 +0200

    Började med createField funktionen

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -3,6 +3,44 @@ height = 16;
 mines = 40;
 
 
+createField = function () {
+
+    xLimit = width + 2;
+    yLimit = height + 2;
+
+    newField = document.createElement("table");
+
+    for (y = 0; y < yLimit; y++) {
+
+        row = document.createElement("tr");
+
+        for (x = 0; x < xLimit; x++) {
+
+            data = document.createElement("td");
+            newDiv = document.createElement("div");
+            dataImg = document.createElement("img");
+
+            imgSrc = document.createAttribute("src");
+
+            if (x == 0 || x == xLimit) {
+
+                
+            }
+            else {
+
+            }
+
+            newDiv.appendChild(dataImg);
+            data.appendChild(newDiv);
+            row.appendChild(data);
+        }
+
+        newField.appendChild(row);
+    }
+
+    document.getElementById("fieldHolder").appendChild(newField);
+}
+
 newPicTest = function (x,y) {
 
 
